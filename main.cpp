ba25468bec5dd9e676994ce594151f896fa799d1
maridilo <146822054+maridilo@users.noreply.github.com>
2023-12-14 12:28:55 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<cstdio>
 #include<cstdlib>
+#include<cstring>
 
 using namespace std;
 
@@ -13,9 +14,27 @@ void mostrarEstudiante(struct Estudiante estudiante){
     printf("Nombre: %s\nEdad: %d\nPromedio: %.2f\n",
            estudiante.nombre, estudiante.edad, estudiante.promedio);
 }
-
-void mostrarEstudiantePuntero(struct Estudiante *estudiante){
-    printf("Nombre: %s\nEdad: %d\nPromedio: %.2f\n",
-           estudiante->nombre, estudiante->edad, estudiante->promedio);
+void guardarEstudiante(struct Estudiante *estudiante){
+    printf("Nombre: ");
+    scanf("%s", estudiante->nombre);
+    printf("Edad: ");
+    scanf("%d", &estudiante->edad);
+    printf("Promedio: ");
+    scanf("%f", &estudiante->promedio);
+}
+void eliminarEstudiante(struct Estudiante *estudiante){
+    strcpy(estudiante->nombre, "");
+    estudiante->edad = 0;
+    estudiante->promedio = 0.0;
 }
 
+int main() {
+    // Declaracion de variables
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+    // Imprimir datos del estudiante
+    mostrarEstudiante(estudiante1);
+    return 0;
+}
