2f2cd420c919bdaaf4adc6c14ca59ab7ef02bb73
maridilo <146822054+maridilo@users.noreply.github.com>
2023-12-14 12:17:55 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include<cstdio>
+#include<cstdlib>
 
+using namespace std;
+
+struct Estudiante{
+    char nombre[50];
+    int edad;
+    char sexo[10];
+    char carrera[50];
+    int ciclo;
+    float promedio;
+};
