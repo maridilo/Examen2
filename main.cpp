372230f4b948fc69d9564168c10086bd6633f898
maridilo <146822054+maridilo@users.noreply.github.com>
2023-12-14 12:13:38 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<cstdlib>
+#include<cstring>
+#include<cstdio>
+
+
+using namespace std;
+
+struct estudiante {
+    string nombre;
+    int edad;
+    float promedio;
+};
+void mostrar (estudiante estudiante);
+
+mostrarEstudiante(estudiante estudiante){
+    cout << "Nombre: " << estudiante.nombre << endl;
+    cout << "Edad: " << estudiante.edad << endl;
+    cout << "Promedio: " << estudiante.promedio << endl;
+}
 
